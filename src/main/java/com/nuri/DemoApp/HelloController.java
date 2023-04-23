42109dfb46720d728cf7c8342bb7318960a4efc2
Prasanth Dey <prasanthdey@gmail.com>
2023-04-23 15:09:25 +0200

    Updated Hello Controller!

--- src/main/java/com/nuri/DemoApp/HelloController.java
+++ src/main/java/com/nuri/DemoApp/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, " + appenv + " new world!";
+        return "Hello, " + appenv + "world!";
     }
 }
